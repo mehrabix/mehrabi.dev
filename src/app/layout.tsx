b90644e98c7168115a83e78dfe3fa46f859d7e0a
mehrabix <mehrabi@post.com>
2023-12-27 09:58:23 +0330

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Ahmad Mehrabi ▲ Frontend Engineer',
-  description: 'Ahmad Mehrabi is a highly skilled frontend engineer based in Turkey, bringing a wealth of expertise and creativity to the world of web development. With a passion for crafting seamless and visually appealing user interfaces, Ahmad possesses a keen eye for design and a deep understanding of the latest frontend technologies.',
+  description: 'Ahmad Mehrabi is a highly skilled frontend engineer based in Turkey, contributing a wealth of expertise and creativity to the field of web development. With a passion for crafting seamless and visually appealing user interfaces, Ahmad possesses a keen proficiency in JavaScript coding and a deep understanding of the latest frontend technologies.',
 }
 
 export default function RootLayout({
