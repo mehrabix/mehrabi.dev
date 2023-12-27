74f0a202196a302bff3ae1ba4ed350ea1f5320c0
mehrabix <mehrabi@post.com>
2023-12-27 08:58:24 +0330

    add nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-full h-screen bg-black flex justify-center items-center">
+        <h1 className="text-white text-2xl">Coming soon...</h1>
+      </div>
+    </>
+  )
+}
