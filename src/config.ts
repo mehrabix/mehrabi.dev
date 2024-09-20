f0556b9e73834e5cfdf1c94138fb2f049da6729d
a.mehrabi <mehrabi@post.com>
2024-09-20 14:00:43 +0330

    update site map

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ahmad Mehrabi | Software Engineer Portfolio';
+export const SITE_TITLE = 'Ahmad Mehrabi | Software Engineer';
 export const SITE_DESCRIPTION = 'Welcome to my portfolio! I am a passionate Software Engineer with expertise in building innovative web applications using JavaScript, TypeScript, React, Java, PL/SQL, Angular and more. Explore my projects, blog, and resume.';
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
