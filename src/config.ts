3158d9141d1e9bd9ffb51f9b415e00b9912d322a
a.mehrabi <mehrabi@post.com>
2024-09-20 13:57:00 +0330

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Ahmad Mehrabi | Software Engineer Portfolio';
+export const SITE_DESCRIPTION = 'Welcome to my portfolio! I am a passionate Software Engineer with expertise in building innovative web applications using JavaScript, TypeScript, React, and more. Explore my projects, blog, and resume.';
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
